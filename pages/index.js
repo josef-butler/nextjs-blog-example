eea8d3f29d808d902335863ecee56ff61a21ba48
Joe Butler <48168036+josef-butler@users.noreply.github.com>
2022-06-08 20:00:35 +1200

    Line change to test deployment

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm testing Next.Js.</p>
+        <p>I'm testing Next.js. It looks quite good.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
